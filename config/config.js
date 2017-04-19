0d3e5ba55ec21f494c9cb98ceec28fdab7a827bb
Nadar <A8859780@ext.saint-gobain.com>
2017-04-19 20:34:03 +0530

    error fixes

--- config/config.js
+++ config/config.js
@@ -1,27 +1,28 @@
 var mysql = require('mysql');
 
 function Connection() {
-  this.pool = null;
+    this.pool = null;
 
-  this.init = function() {
-    this.pool = mysql.createPool({
-      connectionLimit: 10,
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'rest'
-    });
-  };
+    this.init = function() {
+        this.pool = mysql.createPool({
+            connectionLimit: 10,
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'rest'
+        });
+    };
 
-  this.acquire = function(callback) {
-    this.pool.getConnection(function(err, connection) {
-      if(err){
-        connection.release(); 
-        throw err;
-      }
-      callback(err, connection);
-    });
-  };
+    this.acquire = function(callback) {
+        this.pool.getConnection(function(err, connection) {
+            if (err) {
+                console.log(err);
+                connection.release();
+                throw err;
+            }
+            callback(err, connection);
+        });
+    };
 }
 
 module.exports = new Connection();
